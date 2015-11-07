afa1ece0778bd2ab0d3c3800ae062eda5d98e68b
Andrea Richiardi <a.richiardi.work@gmail.com>
2015-11-06 16:34:21 -0800

    Fix some test by using load/js-fake-load.

--- src/cljs/replumb/load.cljs
+++ src/cljs/replumb/load.cljs
@@ -4,6 +4,11 @@
 ;; For now there is no load from file
 
 (defn js-default-load
+  "This load function just calls: (cb nil)"
+  [{:keys [name macros path file] :as full} cb]
+  (cb nil))
+
+(defn js-fake-load
   "This load function just calls:
   (cb {:lang   :js
        :source \"\"})"
