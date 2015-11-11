51d4e82bce5fee9eadd52be9091168ee49803ec8
Mike Fikes <mike@fikesfarm.com>
2015-11-10 23:01:45 -0500

    Add facilities for Node.js
    - Sets up some infrastructure for producing a
      load-fn that will search a given set of src-paths
      for source files, honoring the extension search
      orderings requried by cljs.js for macro and
      runtime files.
    
    Adds some code to:
    
    - Load files from filesystem in Node.js
    - Implement a simple REPL in Node.js

--- src/cljs/replumb/load.cljs
+++ src/cljs/replumb/load.cljs
@@ -1,4 +1,5 @@
-(ns replumb.load)
+(ns replumb.load
+  (:require [clojure.string :as string]))
 
 ;; From mfikes/planck
 ;; For now there is no load from file
@@ -15,3 +16,48 @@
   [_ cb]
   (cb {:lang   :js
        :source ""}))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; File-based load-fn infrastructure ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn- filename->lang
+  "Converts a filename to a lang keyword by inspecting the file
+  extension."
+  [filename]
+  (if (string/ends-with? filename ".js")
+    :js
+    :clj))
+
+(defn- read-some
+  "Reads the first filename in a sequence of supplied filenames,
+  using a supplied read-file-fn, calling back upon first successful
+  read, otherwise calling back with nil."
+  [[filename & more-filenames] read-file-fn cb]
+  (if filename
+    (read-file-fn
+      filename
+      (fn [source]
+        (if source
+          (cb {:lang   (filename->lang filename)
+               :source source})
+          (read-some more-filenames read-file-fn cb))))
+    (cb nil)))
+
+(defn- filenames-to-try
+  "Produces a sequence of filenames to try reading, in the
+  order they should be tried."
+  [src-paths macros path]
+  (let [extensions (if macros
+                     [".clj" ".cljc"]
+                     [".cljs" ".cljc" ".js"])]
+    (for [extension extensions
+          src-path src-paths]
+      (str src-path "/" path extension))))
+
+(defn make-load-fn
+  "Makes a load function that will read from a sequence of src-paths
+  using a supplied read-file-fn."
+  [src-paths read-file-fn]
+  (fn [{:keys [macros path]} cb]
+    (read-some (filenames-to-try src-paths macros path) read-file-fn cb)))
