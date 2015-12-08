de063d5c61b10db1298f4eb6a21793ae173407c0
Andrea Richiardi <a.richiardi.work@gmail.com>
2015-12-08 10:15:40 -0800

    Add node repl out as src-path in order to have (doc clojure.set) working

--- scripts/node-repl.sh
+++ scripts/node-repl.sh
@@ -2,4 +2,4 @@
 
 # For convenience you can pass --verbose here and it will be forwarded to the repl
 node dev-resources/private/node/compiled/nodejs-repl.js $1 \
-     dev-resources/private/test/src/cljs:dev-resources/private/test/src/clj
+     dev-resources/private/node/compiled/out:dev-resources/private/test/src/cljs:dev-resources/private/test/src/clj
