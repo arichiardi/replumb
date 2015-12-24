0c513ef481fed8e41b80bc643bd74e044c5bb280
Andrea Richiardi <a.richiardi.work@gmail.com>
2015-12-23 16:55:00 -0800

    Bring things back to normal manual versioning, 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject replumb/replumb "0.1.4-alpha1-SNAPSHOT"
+(defproject replumb/replumb "0.1.4-SNAPSHOT"
   :description "ClojureScript plumbing for your bootstrapped REPLs."
   :url "https://github.com/clojurex/replumb"
   :license {:name "Eclipse Public License"
