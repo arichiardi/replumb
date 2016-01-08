1e9bfba696127a36a5177d7f5ff11e5b3f54fc59
Andrea Richiardi <a.richiardi.work@gmail.com>
2016-01-08 10:44:08 -0800

    Bump ClojureScript to 1.7.228

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.170"]
+                 [org.clojure/clojurescript "1.7.228"]
                  [org.clojure/tools.reader "1.0.0-alpha2"]]
 
   :plugins [[lein-cljsbuild "1.1.1"]
