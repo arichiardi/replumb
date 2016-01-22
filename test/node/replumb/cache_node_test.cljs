607b23962e1754150d23aa543eccc0169ded1479
Tomasz Biernacki <tomasz.biernacki@scalac.io>
2016-01-22 20:40:35 +0100

    eval-cache tests rework, closes #137

--- test/node/replumb/cache_node_test.cljs
+++ test/node/replumb/cache_node_test.cljs
@@ -9,6 +9,18 @@
             [replumb.load :as load]
             [replumb.nodejs.io :as io]))
 
+(defn rename-file-names-to-tmp
+  [paths]
+  (doseq [path paths]
+    (io/safely-rename-file! path (str path ".tmp"))
+    (is (false? (io/file-exists? path)) (str path " should not exist"))))
+
+(defn revert-file-names
+  [paths]
+  (doseq [path paths]
+    (let [tmp-file (str path ".tmp")]
+      (io/safely-rename-file! tmp-file (apply str (drop-last 4 tmp-file))))))
+
 (let [src-paths ["dev-resources/private/test/node/compiled/out"]
       cache-path "dev-resources/private/test/cache"
       path "clojure/set"
@@ -26,13 +38,43 @@
                                :read-eval-call (partial repl/read-eval-call target-opts validated-echo-cb)
                                :reset-env! (partial repl/reset-env! target-opts)}))]
 
-  ;; assumes there is no cached files in cache-path but yes in src-files
-  (deftest read-no-files-in-cache-path-yes-src-paths
-    (io/safely-delete! set-js-path)
-    (io/safely-delete! set-json-path)
-    (is (false? (io/file-exists? set-js-path)) "clojure_SLASH_set.js should not exist")
-    (is (false? (io/file-exists? set-json-path)) "clojure_SLASH_set.cache.json should not exist")
+  (deftest read-no-files-in-cache-path-no-src-paths
+    (rename-file-names-to-tmp [set-js-path set-json-path])
+    (rename-file-names-to-tmp (flatten (load/cache-file-paths-for-load-fn src-paths macros path)))
+    (let [cache-opts {}
+          {:keys [load-fn]} (fns-from-cache-opts cache-opts)
+          load-res (load-fn)]
+      (is (contains? load-res :source) "Loading should succeed and the resulting map contain :source")
+      (is (contains? load-res :lang) "Loading should succeed and the resulting map contain :lang")
+      (is (= (:lang load-res) :clj) "The resulting language should be clj")
+      (is (false? (contains? load-res :cache)) "The resulting map should not contain :cache."))
 
+    (let [cache-opts {:cache {:path cache-path}}
+          {:keys [load-fn]} (fns-from-cache-opts cache-opts)
+          load-res (load-fn)]
+      (is (contains? load-res :source) "Loading should succeed and the resulting map contain :source")
+      (is (contains? load-res :lang) "Loading should succeed and the resulting map contain :lang")
+      (is (= (:lang load-res) :clj) "The resulting language should be clj")
+      (is (false? (contains? load-res :cache)) "The resulting map should not contain :cache."))
+
+    (let [cache-opts {:cache {:src-paths-lookup? true}}
+          {:keys [load-fn]} (fns-from-cache-opts cache-opts)
+          load-res (load-fn)]
+      (is (contains? load-res :source) "Loading should succeed and the resulting map contain :source")
+      (is (contains? load-res :lang) "Loading should succeed and the resulting map contain :lang")
+      (is (= (:lang load-res) :clj) "The resulting language should be clj")
+      (is (false? (contains? load-res :cache)) "The resulting map should not contain :cache."))
+
+    (let [cache-opts {:cache {:path cache-path :src-paths-lookup? true}}
+          {:keys [load-fn]} (fns-from-cache-opts cache-opts)
+          load-res (load-fn)]
+      (is (contains? load-res :source) "Loading should succeed and the resulting map contain :source")
+      (is (contains? load-res :lang) "Loading should succeed and the resulting map contain :lang")
+      (is (= (:lang load-res) :clj) "The resulting language should be clj")
+      (is (false? (contains? load-res :cache)) "The resulting map should not contain :cache.")))
+
+  (deftest read-no-files-in-cache-path-yes-src-paths
+    (revert-file-names (flatten (load/cache-file-paths-for-load-fn src-paths macros path)))
     (let [cache-opts {}
           {:keys [load-fn]} (fns-from-cache-opts cache-opts)
           load-res (load-fn)]
@@ -79,38 +121,8 @@
       (is (not (re-find #"nil" out)) (str test-expr " should not return nil"))
       (reset-env! '[clojure.set])))
 
-  (deftest no-write-file-fn
-    (let [cache-opts {:cache {:path cache-path} :write-file-fn! nil}
-          {:keys [load-fn read-eval-call reset-env!]} (fns-from-cache-opts cache-opts)
-          res (read-eval-call "(require 'clojure.set)")
-          out (unwrap-result res)
-          load-res (load-fn)]
-      (is (success? res) "(require 'clojure.set) should succeed and write to cache.")
-      (is (valid-eval-result? out) "(require 'clojure.set) should be a valid result")
-      (is (false? (io/file-exists? set-js-path)) "clojure_SLASH_set.js should not exist")
-      (is (false? (io/file-exists? set-json-path)) "clojure_SLASH_set.cache.json should not exist")
-      (is (contains? load-res :source) "Loading should succeed and the resulting map contain :source")
-      (is (contains? load-res :lang) "Loading should succeed and the resulting map contain :lang")
-      (is (= (:lang load-res) :clj) "The resulting language should be clj")
-      (is (false? (contains? load-res :cache)) "The resulting map should not contain :cache.")
-      (reset-env! '[clojure.set])))
-
-  (deftest write-files-to-cache
-    (let [cache-opts {:cache {:path cache-path}}
-          {:keys [read-eval-call reset-env!]} (fns-from-cache-opts cache-opts)
-          res (read-eval-call "(require 'clojure.set)")
-          out (unwrap-result res)]
-      (is (success? res) "(require 'clojure.set) should succeed and write to cache.")
-      (is (valid-eval-result? out) "(require 'clojure.set) should be a valid result")
-      (is (true? (io/file-exists? set-js-path)) "clojure_SLASH_set.js should exist")
-      (is (true? (io/file-exists? set-json-path)) "clojure_SLASH_set.cache.json should exist")
-      (reset-env! '[clojure.set])))
-
-  ;; cached files both in cache-path (json) and src-path (edn)
   (deftest read-yes-files-in-cache-path-yes-src-paths
-    (is (true? (io/file-exists? set-js-path)) "clojure_SLASH_set.js should exist")
-    (is (true? (io/file-exists? set-json-path)) "clojure_SLASH_set.cache.json should exist")
-
+    (revert-file-names [set-js-path set-json-path])
     (let [cache-opts {}
           {:keys [load-fn]} (fns-from-cache-opts cache-opts)
           load-res (load-fn)]
@@ -165,13 +177,7 @@
       (reset-env! '[clojure.set])))
 
   (deftest read-yes-files-in-cache-path-no-src-paths
-    ;; rename the edn file
-    (doseq [[_ cache-path] (load/cache-file-paths-for-load-fn src-paths macros path)]
-      (when (io/file-exists? cache-path)
-        (io/rename-file cache-path (str cache-path ".tmp")))
-      ;(io/safely-delete! cache-path)
-      (is (false? (io/file-exists? cache-path)) (str cache-path " should not exist")))
-
+    (rename-file-names-to-tmp (flatten (load/cache-file-paths-for-load-fn src-paths macros path)))
     (let [cache-opts {}
           {:keys [load-fn]} (fns-from-cache-opts cache-opts)
           load-res (load-fn)]
@@ -216,61 +222,50 @@
       (is (success? res) (str test-expr " should succeed"))
       (is (valid-eval-result? out) (str test-expr " should be a valid result"))
       (is (not (re-find #"nil" out)) (str test-expr " should not return nil"))
+      (reset-env! '[clojure.set]))
+    (revert-file-names (flatten (load/cache-file-paths-for-load-fn src-paths macros path))))
+
+  (deftest no-write-file-fn
+    (rename-file-names-to-tmp [set-js-path set-json-path])
+    (let [cache-opts {:cache {:path cache-path} :write-file-fn! nil}
+          {:keys [load-fn read-eval-call reset-env!]} (fns-from-cache-opts cache-opts)
+          res (read-eval-call "(require 'clojure.set)")
+          out (unwrap-result res)
+          load-res (load-fn)]
+      (is (success? res) "(require 'clojure.set) should succeed and write to cache.")
+      (is (valid-eval-result? out) "(require 'clojure.set) should be a valid result")
+      (is (false? (io/file-exists? set-js-path)) "clojure_SLASH_set.js should not exist")
+      (is (false? (io/file-exists? set-json-path)) "clojure_SLASH_set.cache.json should not exist")
+      (is (contains? load-res :source) "Loading should succeed and the resulting map contain :source")
+      (is (contains? load-res :lang) "Loading should succeed and the resulting map contain :lang")
+      (is (= (:lang load-res) :clj) "The resulting language should be clj")
+      (is (false? (contains? load-res :cache)) "The resulting map should not contain :cache.")
       (reset-env! '[clojure.set])))
 
-  (deftest read-no-files-in-cache-path-no-src-paths
-    (io/safely-delete! set-js-path)
-    (io/safely-delete! set-json-path)
-    (is (false? (io/file-exists? set-js-path)) "clojure_SLASH_set.js should not exist")
-    (is (false? (io/file-exists? set-json-path)) "clojure_SLASH_set.cache.json should not exist")
-    (doseq [[_ cache-path] (load/cache-file-paths-for-load-fn src-paths macros path)]
-      (is (false? (io/file-exists? cache-path)) (str cache-path " should not exist")))
-
-    (let [cache-opts {}
-          {:keys [load-fn]} (fns-from-cache-opts cache-opts)
-          load-res (load-fn)]
-      (is (contains? load-res :source) "Loading should succeed and the resulting map contain :source")
-      (is (contains? load-res :lang) "Loading should succeed and the resulting map contain :lang")
-      (is (= (:lang load-res) :clj) "The resulting language should be clj")
-      (is (false? (contains? load-res :cache)) "The resulting map should not contain :cache."))
-
+  (deftest write-files-to-cache
     (let [cache-opts {:cache {:path cache-path}}
-          {:keys [load-fn]} (fns-from-cache-opts cache-opts)
-          load-res (load-fn)]
-      (is (contains? load-res :source) "Loading should succeed and the resulting map contain :source")
-      (is (contains? load-res :lang) "Loading should succeed and the resulting map contain :lang")
-      (is (= (:lang load-res) :clj) "The resulting language should be clj")
-      (is (false? (contains? load-res :cache)) "The resulting map should not contain :cache."))
+          {:keys [read-eval-call reset-env!]} (fns-from-cache-opts cache-opts)
+          res (read-eval-call "(require 'clojure.set)")
+          out (unwrap-result res)]
+      (is (success? res) "(require 'clojure.set) should succeed and write to cache.")
+      (is (valid-eval-result? out) "(require 'clojure.set) should be a valid result")
+      (is (true? (io/file-exists? set-js-path)) "clojure_SLASH_set.js should exist")
+      (is (true? (io/file-exists? set-json-path)) "clojure_SLASH_set.cache.json should exist")
 
-    (let [cache-opts {:cache {:src-paths-lookup? true}}
-          {:keys [load-fn]} (fns-from-cache-opts cache-opts)
-          load-res (load-fn)]
-      (is (contains? load-res :source) "Loading should succeed and the resulting map contain :source")
-      (is (contains? load-res :lang) "Loading should succeed and the resulting map contain :lang")
-      (is (= (:lang load-res) :clj) "The resulting language should be clj")
-      (is (false? (contains? load-res :cache)) "The resulting map should not contain :cache."))
+      ;; delete the generated files because could be different from the commited version
+      ;; leaving the git repo with modified files
+      (io/safely-delete! set-js-path)
+      (io/safely-delete! set-json-path)
 
-    (let [cache-opts {:cache {:path cache-path :src-paths-lookup? true}}
-          {:keys [load-fn]} (fns-from-cache-opts cache-opts)
-          load-res (load-fn)]
-      (is (contains? load-res :source) "Loading should succeed and the resulting map contain :source")
-      (is (contains? load-res :lang) "Loading should succeed and the resulting map contain :lang")
-      (is (= (:lang load-res) :clj) "The resulting language should be clj")
-      (is (false? (contains? load-res :cache)) "The resulting map should not contain :cache."))
-
-    ;; revert the original edn file
-    (doseq [[_ cache-path] (load/cache-file-paths-for-load-fn src-paths macros path)]
-      (let [tmp-file (str cache-path ".tmp")]
-        (when (io/file-exists? tmp-file)
-          (io/rename-file tmp-file (apply str (drop-last 4 tmp-file)))
-          (is (true? (io/file-exists? cache-path)) (str cache-path " should exist")))))))
+      (revert-file-names [set-js-path set-json-path])
+      (reset-env! '[clojure.set]))))
 
 ;;; the order in this case is important
 (defn test-ns-hook []
   (repl/force-init!)
+  (read-no-files-in-cache-path-no-src-paths)
   (read-no-files-in-cache-path-yes-src-paths)
-  (no-write-file-fn)
-  (write-files-to-cache)
   (read-yes-files-in-cache-path-yes-src-paths)
   (read-yes-files-in-cache-path-no-src-paths)
-  (read-no-files-in-cache-path-no-src-paths))
+  (no-write-file-fn)
+  (write-files-to-cache))
