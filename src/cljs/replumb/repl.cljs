e4c6cb7bb8ef8bc0ba3e13bf643323e43f1d01b4
Andrea Richiardi <a.richiardi.work@gmail.com>
2016-02-19 15:38:14 -0800

    Restore pprint in initialization function, closes #142

--- src/cljs/replumb/repl.cljs
+++ src/cljs/replumb/repl.cljs
@@ -850,7 +850,7 @@
   Data is passed from outside and will be forwarded to :init-fn!."
   [opts data]
   (when (:verbose opts)
-    (common/debug-prn "Initializing REPL environment with data" (println data)))
+    (common/debug-prn "Initializing REPL environment with data" (with-out-str (pprint data))))
 
   ;; Target/user init, we need at least one init-fn, the default init function
   (let [init-fns (:init-fns opts)]
