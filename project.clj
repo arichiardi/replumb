58e5cdfea03ff648b0251ae11cc71057d955166a
Andrea Richiardi <a.richiardi.work@gmail.com>
2016-02-22 18:40:09 -0800

    Version 0.1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject replumb/replumb "0.1.5-SNAPSHOT"
+(defproject replumb/replumb "0.1.5"
   :description "ClojureScript plumbing for your bootstrapped REPLs."
   :url "https://github.com/clojurex/replumb"
   :license {:name "Eclipse Public License"
