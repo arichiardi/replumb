5b5db686b64551edb8dfeacef66e3270ae1a1bd1
Andrea Richiardi <a.richiardi.work@gmail.com>
2016-04-11 17:46:29 -0700

    Setup node repl with :optimizations :simple

--- scripts/node-repl.sh
+++ scripts/node-repl.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+JS=dev-resources/private/node/js/compiled/nodejs-repl.js
+VERBOSE=
+
+if [ "$1" == "--simple" ]; then
+    JS=dev-resources/private/node/js/simple/compiled/nodejs-repl.js
+fi
+
+if [[ "$1" == "--verbose" ]] || [[ $2 == "--verbose" ]]
+then
+    VERBOSE=--verbose
+fi
+
 # For convenience you can pass --verbose here and it will be forwarded to the repl
-node dev-resources/private/node/js/compiled/nodejs-repl.js $1 \
+node $JS $VERBOSE \
      dev-resources/private/node/js/compiled/out:dev-resources/private/test/src/cljs:dev-resources/private/test/src/clj:dev-resources/private/test/src/cljc:dev-resources/private/test/src/js
