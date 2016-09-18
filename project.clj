c03bcba20915fc84ec96edb4b223fcaaeb036619
Andrea Richiardi <a.richiardi.work@gmail.com>
2016-09-17 19:53:40 -0700

    Bump clojurescript and tools.reader

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha11"]
-                 [org.clojure/clojurescript "1.9.225" :scope "test"]
-                 [org.clojure/tools.reader "1.0.0-alpha3" :scope "test"]
+                 [org.clojure/clojurescript "1.9.229" :scope "test"]
+                 [org.clojure/tools.reader "1.0.0-beta1" :scope "test"]
                  [com.cognitect/transit-clj "0.8.285" :scope "test"]
                  [com.cognitect/transit-cljs "0.8.239" :scope "test"]]
 
@@ -179,14 +179,11 @@
                    :source-paths ["src/cljs" "src/clj" "test/clj" "test/cljs" "test/browser" "src/browser" "repl-demo/browser/cljs" "dev"]
                    :dependencies [[com.cemerick/piggieback "0.2.1"]
                                   [org.clojure/tools.nrepl "0.2.12"]
-                                  [figwheel-sidecar "0.5.4-7"]
+                                  [figwheel-sidecar "0.5.7"]
                                   [cljsjs/jqconsole "2.13.2-0"]
-                                  [reagent "0.5.1"]
-                                  [binaryage/devtools "0.5.2"]
-                                  [spellhouse/clairvoyant "0.0-72-g15e1e44"]]
+                                  [reagent "0.6.0-rc"]
+                                  [binaryage/devtools "0.8.1"]]
                    :plugins [[lein-doo "0.1.7-SNAPSHOT"]
-                             [lein-figwheel "0.5.4-7" :exclusions [cider/cider-nrepl]]
+                             [lein-figwheel "0.5.7" :exclusions [cider/cider-nrepl]]
                              [lein-shell "0.4.2"]]}
-             :repl {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]
-                    :repl-options {:nrepl-middleware [#_cider.nrepl/cider-middleware
-                                                      cemerick.piggieback/wrap-cljs-repl]}}})
+             :repl {:repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})
